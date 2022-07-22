a557402bbf5e61d64575106ab8b130c133728fe4
Sarah Menu <sarah.menu@gmail.com>
2022-07-22 20:37:52 +0200

    get middle character

---
+++ 7kyu/get_the_middle_character.rb
@@ -0,0 +1,13 @@
+=begin
+You are going to be given a word.
+Your job is to return the middle character of the word.
+If the word's length is odd, return the middle character.
+If the word's length is even, return the middle 2 characters.
+=end
+def get_middle(s)
+  if s.length % 2 == 0
+    s[s.length / 2 - 1] + s[s.length / 2]
+  else
+    s[s.length / 2]
+  end
+end
